a68107e9d6a14d6e1d7bd49195074dd8fab6aaf8
ds1sqe <ds1sqe@mensakorea.org>
2024-03-19 11:41:58 +0900

    add evaluator. add evaluation of integer, boolean, infix/prefix expression and if-expression

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"mylang/evaluator"
 	"mylang/lexer"
 	"mylang/parser"
 )
@@ -33,8 +34,13 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+
+		}
+
 	}
 }
 
